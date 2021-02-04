15c04026c633e674fa717b86185161ff4a2cd7d7
LBlend <lblend@protonmail.com>
2021-02-04 20:29:54 +0100

    added golang

---
+++ Go/fizzbuzz.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i < 101; i++ {
+		var output = ""
+
+		if i % 3 == 0 {
+			output += "Fizz"
+		}
+		if i % 5 == 0 {
+			output += "Buzz"
+		}
+
+		if output == "" {
+			fmt.Println(i)
+			continue
+		}
+
+		fmt.Println(output)
+	} 
+}
